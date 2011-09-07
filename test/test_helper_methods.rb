64a04b47f7a163507c62c248d15f2b5ac60a2bfb
janfri <janfri26@gmail.com>
2011-09-07 20:42:51 +0200

    Add testing for helper methods of Rim.

---
+++ test/test_helper_methods.rb
@@ -0,0 +1,26 @@
+# -- encoding: utf-8 --
+require 'rim'
+require 'test/unit'
+
+class TestHelperMethods < Test::Unit::TestCase
+
+  class  ::Rim
+    attr_accessor :some_new_attr
+  end
+
+  def test_attr_accessor
+    rim = Rim.instance
+    assert_nil rim.some_new_attr, 'New attribute must be nil.'
+    rim.some_new_attr = :standard_setter
+    assert_equal :standard_setter, rim.some_new_attr
+    rim.some_new_attr :advanced_setter
+    assert_equal :advanced_setter, rim.some_new_attr
+  end
+
+  def test_filelist
+    rim = Rim.instance
+    fl = rim.filelist(/rakefile/i, 'lib')
+    assert_equal %w(Rakefile lib), fl.to_a
+  end
+
+end
