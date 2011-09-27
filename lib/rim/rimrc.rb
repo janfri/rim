9811443754d11018412d5a39be6c8ebc6153f92c
Jan Friedrich <janfri26@gmail.com>
2011-09-27 10:11:10 +0200

    Add rimrc task.

---
+++ lib/rim/rimrc.rb
@@ -0,0 +1,13 @@
+# -- encoding: utf-8 --
+
+# Load ~/.rimrc if it exist
+home = if Dir.respond_to? :home
+         Dir.home
+       else
+         # This will hopefully work on *NIX and Windows systems
+         ENV['HOME'] || ENV['HOMEDRIVE'] + ENV['HOMEPATH'] || ENV['USERPROFILE']
+       end
+rimrc = File.join(home, '.rimrc')
+if File.exist?(rimrc)
+  load rimrc
+end
