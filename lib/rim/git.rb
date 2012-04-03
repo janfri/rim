5b70f32ef1f85d2dddb8e63609f252b14fda020e
Jan Friedrich <janfri26@gmail.com>
2012-04-03 12:08:19 +0200

    New task git:push.

--- lib/rim/git.rb
+++ lib/rim/git.rb
@@ -1,6 +1,15 @@
 # -- encoding: utf-8 --
 require 'rim/release'
 
+class Rim
+  # Git remotes to push (default: <tt>origin</tt>)
+  attr_accessor :git_remotes
+end
+
+Rim.defaults do
+  git_remotes %w(origin)
+end
+
 Rim.after_setup do
   if Dir.exist?('.git')
     tag_name = "v#{version}"
@@ -8,7 +17,6 @@ Rim.after_setup do
       desc "Create git tag #{tag_name}"
       tag_task = task :tag do
         cmd = "git tag #{tag_name}"
-        puts cmd
         sh cmd
       end
       desc 'Check if git tree is clean'
@@ -16,6 +24,12 @@ Rim.after_setup do
         res = `git status`
         if res !~ /nothing to commit/
           raise 'Git tree is not clean'
+        end
+      end
+      desc 'Push master to git remotes'
+      task :push => :check do
+        git_remotes.each do |remote|
+          sh "git push #{remote} master"
         end
       end
     end
