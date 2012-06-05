aa86d50a9bf17b290a9363faa71427dc09e3cd0c
Jan Friedrich <janfri26@gmail.com>
2012-06-05 22:31:42 +0200

    New option test_require_paths for test task. Release 1.6.1.

--- lib/rim/test.rb
+++ lib/rim/test.rb
@@ -2,10 +2,14 @@
 class Rim
   # Test files (default: <code>'test/\*\*/\*.rb')</code>
   attr_accessor :test_files
+
+  # Require dirs for tests (default: <code>%w(lib test)</code>)
+  attr_accessor :test_require_paths
 end
 
 Rim.defaults do
   test_files filelist('test/**/*.rb')
+  test_require_paths %w(lib test)
 end
 
 Rim.after_setup do
@@ -13,6 +17,7 @@ Rim.after_setup do
     require 'rake/testtask'
     Rake::TestTask.new do |t|
       t.test_files = test_files
+      t.libs << test_require_paths
     end
     task :default => :test
   end
