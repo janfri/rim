f26385a67a3634733cad14509378b9e1dadb9cc9
Jan Friedrich <janfri26@gmail.com>
2014-02-26 21:50:29 +0100

    Expand FileList instances in info task.

--- lib/rim/info.rb
+++ lib/rim/info.rb
@@ -3,6 +3,15 @@ Rim.after_setup do
   desc 'Prints informations about the Rim object.'
   task :info do
     require 'yaml'
-    puts Rim.instance.to_yaml
+    info = {}
+    Rim.instance.instance_variables.sort.each do |attr|
+      name = attr.to_s.sub(/^@/, '')
+      val = send(name)
+      if val.kind_of? FileList
+        val = val.to_a
+      end
+      info[name] = val
+    end
+    puts info.to_yaml
   end
 end
