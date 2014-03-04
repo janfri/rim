d46f4b8b02d0331647161a200cffed90835e8637
Jan Friedrich <janfri26@gmail.com>
2014-03-04 18:27:17 +0100

    Adding rim/check_version again with a fail message to avoid approve backwards compatibility.

--- lib/rim.rb
+++ lib/rim.rb
@@ -9,7 +9,7 @@ require 'singleton'
 # versions as possible and is easy to extend. Feel free to dislike it. ;)
 class Rim
 
-  VERSION = '2.2.0'
+  VERSION = '2.2.1'
 
   begin
     require 'rake/dsl_definition'
---
+++ lib/rim/check_version.rb
@@ -0,0 +1,12 @@
+# -- encoding: utf-8 --
+Rim.after_setup do
+  task :check_version do
+    fail "Task check_version is obsolete, use version:check!"
+  end
+  if feature_loaded? 'rim/gem'
+    task :gem => 'version:check'
+  end
+  if feature_loaded? 'rim/release'
+    task :release => 'version:check'
+  end
+end
