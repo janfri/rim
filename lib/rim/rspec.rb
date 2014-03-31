036af423f366fe2c8bf4ce22e7ec8e60181e6592
Jan Friedrich <janfri26@gmail.com>
2014-03-31 21:22:01 +0200

    Add new tasks rspec and yard.

---
+++ lib/rim/rspec.rb
@@ -0,0 +1,28 @@
+# -- encoding: utf-8 --
+class Rim
+  # RSpec files (default: <code>'spec/**/*.rb')</code>
+  attr_accessor :rspec_files
+
+  # Require dirs for tests (default: <code>%w(lib spec)</code>)
+  attr_accessor :rspec_require_paths
+end
+
+Rim.defaults do
+  rspec_files filelist('spec/**/*.rb')
+  rspec_require_paths %w(lib spec)
+end
+
+Rim.after_setup do
+  unless Array(rspec_files).empty?
+    task :rspec do
+      sh "rspec -I #{rspec_require_paths.join(':')} #{rspec_files}"
+    end
+    task :default => :rspec
+    if feature_loaded? 'rim/gem'
+      task :gem => :rspec
+    end
+    if feature_loaded? 'rim/release'
+      task :release => :rspec
+    end
+  end
+end
---
+++ lib/rim/yard.rb
@@ -0,0 +1,6 @@
+# -- encoding: utf-8 --
+CLEAN.include(FileList['.yardoc'].existing)
+CLOBBER.include(FileList['doc'].existing)
+task :yard do
+  sh 'yard'
+end
