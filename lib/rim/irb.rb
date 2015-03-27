c3308d7df8bad25c56b368414aeaedc3efa76de6
Jan Friedrich <janfri26@gmail.com>
2015-03-27 17:31:13 +0100

    New option irb_requires.

--- lib/rim/irb.rb
+++ lib/rim/irb.rb
@@ -1,8 +1,18 @@
 # -- encoding: utf-8 --
+class Rim
+  # Required files for irb (default: Rim#name)
+  attr_accessor :irb_requires
+end
+
+Rim.defaults do
+  irb_requires name
+end
+
 Rim.after_setup do
   desc 'Start an irb session and loading lib'
   task :irb do
     i_params = Array(require_paths).map {|e| '-I ' << e}.join(' ')
-    sh "irb #{i_params} -r #{name}"
+    r_params = Array(irb_requires).map {|e| '-r' << e}.join(' ')
+    sh "irb #{i_params} #{r_params}"
   end
 end
