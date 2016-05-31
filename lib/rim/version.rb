01534407daccb90bfdb92bc08da7f83bc4d092b6
Jan Friedrich <janfri26@gmail.com>
2016-05-31 10:08:48 +0200

    Check version in gemspec file in version:check task if gem is used and improve error messages.

--- lib/rim/version.rb
+++ lib/rim/version.rb
@@ -2,8 +2,14 @@
 Rim.after_setup do
   namespace :version do
     changelog = filelist(/^Changelog/i).first
-    desc "Check if version in #{changelog} is equal to Rim#version."
+    fail 'No Changelog file found' unless changelog
+    if feature_loaded? 'rim/gem'
+      desc "Check if Rim#version is equal to version in #{changelog} and #{gemspec_file}."
+    else
+      desc "Check if Rim#version is equal to version in #{changelog}."
+    end
     task :check do
+      # changelog
       line = File.open(changelog) do |f|
         f.readline
       end
@@ -13,6 +19,21 @@ Rim.after_setup do
         end
       else
         fail "No version information in #{changelog}"
+      end
+      # gemspec
+      fail 'No gemspec file found' unless File.exist? gemspec_file
+      re = /s\.version ?= ['"](\d+\.\d+\.\d+.*)['"]/o
+      match = File.readlines(gemspec_file).grep(re)
+      case match.size
+      when 1
+        match[0] =~ re
+        if $1 != version
+          fail "Version error (#{gemspec_file}: #$1, Rim#version: #{version})\nTry rake gem:spec"
+        end
+      when 0
+        fail "No version information in #{gemspec_file}"
+      else
+        fail "More than one version information in #{gemspec_file}"
       end
     end
   end
