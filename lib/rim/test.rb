3e2a89d0e5e7e93a4f0736cdbd9b03ef370eb5e1
Jan Friedrich <janfri26@gmail.com>
2016-06-06 10:31:07 +0200

    Add attributes test_warning and test_verbose.

--- lib/rim/test.rb
+++ lib/rim/test.rb
@@ -5,11 +5,19 @@ class Rim
 
   # Require dirs for tests (default: <code>%w(lib test)</code>)
   attr_accessor :test_require_paths
+
+  # Warning option for test task (default <code>true</code>)
+  attr_accessor :test_warning
+
+  # Verbose option for test task (default <code>true</code>)
+  attr_accessor :test_verbose
 end
 
 Rim.defaults do
   test_files filelist('test/**/*.rb')
   test_require_paths %w(lib test)
+  test_warning true
+  test_verbose true
 end
 
 Rim.after_setup do
@@ -18,6 +26,8 @@ Rim.after_setup do
     Rake::TestTask.new do |t|
       t.test_files = test_files
       t.libs << test_require_paths
+      t.warning = test_warning
+      t.verbose = test_verbose
     end
     task :default => :test
 
