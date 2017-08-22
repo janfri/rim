b2cf556c66f07d7f0ea51ee3b5f0a4b6eb76862f
Jan Friedrich <janfri26@gmail.com>
2017-08-22 12:22:08 +0200

    Generalize regtest execution.rb.

--- regtest/execution.rb
+++ regtest/execution.rb
@@ -1,11 +1,12 @@
 # -- encoding: utf-8 --
 require 'regtest'
 
+`rake clobber`
 `rake -T`.split(/\n/).map {|line| line.split(/#/).first.strip}.each do |cmd|
   next if cmd =~ /(regtest)|(irb)|(pry)|(ripl)/
   cmd.sub! /rake/, 'rake -n'
   Regtest.sample cmd do
-    res = `#{cmd} 2>&1`
+    res = `#{cmd} 2>&1`.gsub %r{(?<=pkg/rim-)(.+)(?=\.gem)}, 'x.x.x'
     res.split(/\n/).grep(/Execute/)
   end
 end
