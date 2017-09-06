e6e780cb07f3ed5c409d0a8dcb760bea061388f8
Jan Friedrich <janfri26@gmail.com>
2017-09-06 09:39:09 +0200

    Prepare release 2.13.1.

--- lib/rim.rb
+++ lib/rim.rb
@@ -9,7 +9,7 @@ require 'singleton'
 # versions as possible and is easy to extend. Feel free to dislike it. ;)
 class Rim
 
-  VERSION = '2.13.0'
+  VERSION = '2.13.1'
 
   begin
     require 'rake/dsl_definition'
