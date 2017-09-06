77fe7b426e62e0875e09969af17d442a059f9b3e
Jan Friedrich <janfri26@gmail.com>
2017-09-06 09:37:07 +0200

    Fix description of Rake tasks prepare_release and release.

--- lib/rim/release.rb
+++ lib/rim/release.rb
@@ -1,8 +1,11 @@
 # -- encoding: utf-8 --
 Rim.after_setup do
   if version
-    desc "Release version #{version}"
+    desc "Prepare release version #{version}"
   end
   task :prepare_release
+  if version
+    desc "Release version #{version}"
+  end
   task :release
 end
