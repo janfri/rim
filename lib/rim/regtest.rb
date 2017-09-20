195a687f04fc3254523cdce68dd883583812f912
Jan Friedrich <janfri26@gmail.com>
2017-09-20 19:07:17 +0200

    Determine Regtest version and add it to development dependencies only if it is not yet set in Rakefile.

--- lib/rim/regtest.rb
+++ lib/rim/regtest.rb
@@ -18,7 +18,15 @@ end
 Rim.after_setup do
   if feature_loaded? 'rim/gem'
     self.gem_files += regtest_files
-    development_dependencies << %w(regtest ~>1.0)
+    unless development_dependencies.flatten.include? 'regtest'
+      begin
+        require 'regtest/version'
+        v = Regtest::VERSION.sub(/\.\d+(?:\.?\w+)?$/, '')
+        development_dependencies << %W(regtest ~>#{v})
+      rescue LoadError
+        development_dependencies << %w(regtest ~>1.0)
+      end
+    end
   end
   if regtest_files_rb != REGTEST_FILES_RB
     (::REGTEST_FILES_RB.clear << regtest_files_rb).flatten!
