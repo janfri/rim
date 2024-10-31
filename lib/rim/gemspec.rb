ef4b5a71e00450453b01c4428f9e92f686d5211f
Jan Friedrich <janfri26@gmail.com>
2024-10-31 15:24:35 +0100

    Deprecate accessors of older Rim versions (< 3.0)

--- lib/rim/gemspec.rb
+++ lib/rim/gemspec.rb
@@ -2,6 +2,15 @@
 
 class Rim
 
+  # Deprecate accessors of older Rim versions (< 3.0)
+  %w(author authors bindir dependencies description development_dependencies email executables gem_files homepage install_message license name require_paths requirements ruby_version summary version).each do |attr|
+    define_method attr do |*args|
+      warn "WARNING: Rim accessor #{attr} is not longer supported, define the value in #{gemspec_file}"
+      []
+    end
+    alias_method "#{attr}=", attr
+  end
+
   def gemspec_file
     @gemspec_file ||= Dir['*.gemspec'].first or fail 'no gemspec file found'
   end
